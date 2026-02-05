8e520427b9eec108f742027fdd26590b97687e9f
Manju Tamanampudi <manjukarri3@gmail.com>
2026-02-05 23:24:39 +0530

    one more commit

--- 06-logs.sh
+++ 06-logs.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+logs_folder=/var/logs/shell-practice
+logs_file=/var/logs/shell-practice/$0.sh
+
+
 if [ $(id -u) -ne 0 ]; then
 echo "please login to sudo user"
 exit 1
 fi
+sudo mkdir -p $logs_folder
 
 validate(){
     if [ $1 -ne 0 ]; then
@@ -14,14 +19,14 @@ validate(){
     fi
 
 }
-dnf install nginx -y 
+dnf install nginx -y &>> $logs_file
 validate $? "nginx installed"
 echo "nginx installed ..."
 
-dnf install mysql -y
+dnf install mysql -y &>> $logs_file
 validate $? "mysql installed"
 echo "mysql installed ..."
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $logs_file
 validate $? "node installed"
 echo "node installed ..."
