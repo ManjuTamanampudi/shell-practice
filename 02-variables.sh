4219e619afb6b53ceef546b5319ea53a104759ae
Manju Tamanampudi <manjukarri3@gmail.com>
2026-02-05 20:51:19 +0530

    one more commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 DATE1=$(date +%s)
 
-echo "Hey manju today date is  :" $DATE1
-echo "enter day:" 
-read DAY
-echo "Hey manju today day is  :" $DAY
-sleep 10 
+#echo "Hey manju today date is  :" $DATE1
+#echo "enter day:" 
+#read DAY
+#echo "Hey manju today day is  :" $DAY
+#sleep 10 
 
-DATE2=$(date +%s)
-echo "date is  :" $DATE2
+#DATE2=$(date +%s)
+#echo "date is  :" $
+
+
+
+echo "all args : " $@
+echo "no args : " $#
+echo "pwd : " $PWD
+echo "user : " $USER
+echo "home : " $Home
+echo "pid : " $$
+echo "background process : " sleep 10 &
+echo "backgroundpid : " $!
+
+
